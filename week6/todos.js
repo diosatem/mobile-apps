bafcb6b7fd461545fdf230d7264cdfcb1e33acd4
diosatem <jam19021@byui.edu>
2022-06-04 13:01:59 +0800

    Co-authored-by: Griffin <grriffin@users.noreply.github.com>

--- week6/todos.js
+++ week6/todos.js
@@ -6,17 +6,7 @@ const btnAddTask = document.getElementById("add-task-btn");
 const filter = document.getElementById("filter");
 const taskList = document.getElementById("ul-list");
 const taskListItem = document.getElementById("li-task");
-
-
-
-let taskForm = document.getElementById("task-form");
-const taskNumber = document.getElementById("task-number");
-const btnDeleteTask = document.getElementById("delete-task");
-const taskItem = document.getElementById("item");
-const checkItem = document.getElementById("check-task");
-const completeItem = document.getElementById("complete");
 const alertMsg = document.getElementById("alert-msg");
-
 
 //Event listeners
 document.addEventListener("DOMContentLoaded", displayItems);
@@ -26,8 +16,15 @@ filter.addEventListener("click", filterItems);
 
 //Functions
 function addItems(event) {
-    // event.preventDefault();
-    taskList.innerHTML += `<li id="li-task" class="li-task"><input type="checkbox" id="check-task" class="check-task" name="task" value="task" onclick=""> <label for="check-task">${userInput.value}</label> <button class="delete-task" onclick="deleteItems()">X</button></li>`;
+    if (userInput.value.trim() != "") {
+        alertMsg.innerHTML = "Task successfully added.";
+        alertMsg.style.color = "green";
+
+    } else {
+        alertMsg.innerHTML = "Add a task!";
+        alertMsg.style.color = "red";
+    }
+    taskList.innerHTML += `<li id="li-task" class="li-task"><input type="checkbox" id="check-task" class="check-task" name="task" value="task" onclick=""> <label for="check-task">${userInput.value}</label> <button class="delete-task">X</button></li>`;
     saveToLocal(userInput.value);
     userInput.value = "";
 }
@@ -36,7 +33,7 @@ function deleteItems(event) {
     const item = event.target;
     if (item.classList[0] === "delete-task") {
         const removeLi = item.parentElement;
-        // deleteFromLocal(item);
+        deleteFromLocal(item);
         removeLi.remove();
     }
     if (item.classList[0] === "check-task") {
@@ -46,16 +43,30 @@ function deleteItems(event) {
 }
 
 function filterItems(event) {
-    const listChildNodes = [...taskList.childNodes];
-    console.log(listChildNodes);
-    listChildNodes.filter(function (taskNode) {
-        if (taskNode.classList != "completed") {
-            console.log("completed");
-            console.log(taskNode);
-        } else {
-            console.log("not completed");
+    const listChildNodes = [...taskList.children];
+    for (const nodeItem of listChildNodes) {
+        switch (event.target.className) {
+            case "all":
+                nodeItem.style.display = "flex";               
+                break;
+
+            case "completed":
+                if (nodeItem.classList.contains("completed")) {
+                    nodeItem.style.display = "flex";
+                } else {
+                    nodeItem.style.display = "none";
+                }
+                break;
+
+            case "active":
+                if (!nodeItem.classList.contains("completed")) {
+                    nodeItem.style.display = "flex";
+                } else {
+                    nodeItem.style.display = "none";
+                }
+                break;
         }
-    });
+    }
 }
 
 function saveToLocal(item) {
@@ -67,9 +78,10 @@ function saveToLocal(item) {
     }
     items.push(item);
     localStorage.setItem("items", JSON.stringify(items));
+    console.log(items);
 }
 
-function displayItems() {
+function displayItems(item) {
     let items;
     if (localStorage.getItem("items") === null) {
         items = [];
@@ -77,7 +89,7 @@ function displayItems() {
         items = JSON.parse(localStorage.getItem("items"));;
     }
     items.forEach(function (item) {
-        taskList.innerHTML += `<li id="li-task" class="li-task"><input type="checkbox" id="check-task" class="check-task" name="task" value="task" onclick=""> <label for="check-task">${item.value}</label> <button class="delete-task" onclick="deleteItems()">X</button></li>`;
+        taskList.innerHTML += `<li id="li-task" class="li-task"><input type="checkbox" id="check-task" class="check-task" name="task" value="task" onclick=""> <label for="check-task">${item}</label> <button class="delete-task" onclick="deleteItems()">X</button></li>`;
     });
 }
 
@@ -89,88 +101,7 @@ function deleteFromLocal(item) {
         items = JSON.parse(localStorage.getItem("items"));;
     }
 
-    const itemsIndex = items.children[0].innerText;
+    const itemsIndex = item.children[0];
     items.splice(items.indexOf(itemsIndex), 1);
     localStorage.setItem("items", JSON.stringify(items));
 }
-
-
-// switch(event.target.className) {
-//     case "all":
-//         liTask.style.display = "flex";
-//     console.log("hello");
-//         break;
-
-//     case "completed":
-//         if (taskList.classList.contains("completed")) {
-//             liTask.style.display = "flex";
-//         } else {
-//             liTask.style.display = "none";
-//         }
-//         break;
-
-//     case "active":
-//         if (!liTask.classList.contains("completed")) {
-//             liTask.style.display = "flex";
-//         } else {
-//             liTask.style.display = "none";
-//         }
-//         break;
-// }
-//    }
-// }
-
-
-
-
-// window.onload = checkInput();
-
-// function checkInput() {
-//     if (getTask != null) {
-//         items = getTask;
-//         displayTask();
-//     }  
-// }
-
-// btnAddTask.onclick = addItems => {    
-//     if (userInput.value.trim() != "") {        
-//         items.push(userInput.value.trim());
-//         console.log(items);
-//         console.log(localStorage);
-//         alertMsg.innerHTML = "Task successfully added.";
-//         alertMsg.style.color = "green";
-//         if (getTask == null) {
-//             setTask;           
-//         } else {
-//             setTask;            
-//         }
-//         displayTask();
-
-//     } else {
-//         alertMsg.innerHTML = "Add a task!";
-//         alertMsg.style.color = "red";
-//     }
-// }
-
-// function displayTask() {    
-//     taskList.innerHTML = "";    
-//     for (let i = 0; i < items.length; i++) { 
-//         taskList.innerHTML += `<li class="li-task"><input type="checkbox" id="check-task" class="check-task" name="task" value="task" onclick="(${[i]})"> <label for="check-task">${items[i]}</label> <button id="delete-task" onclick="deleteTask(${[i]})">X</button></li>`;
-//     }
-
-// taskNumber.innerHTML = `${items.length} tasks left`;
-// }
-
-// function deleteTask(index) {
-//     items.splice(index, 1);
-//     if (getTask == null) {
-//         setTask;
-//     } else {
-//         setTask;
-//     }
-//     displayTask();
-// }
-
-// checkItem.onclick = () => {
-//     completeItem;
-// }
